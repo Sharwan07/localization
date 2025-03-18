8ae73655965c91687e6209b4bf7b2281ff03d2d4
Sharwan Kumawat <sharwankumawat2002@gmail.com>
2025-03-18 19:00:40 +0530

    Set languge

---
+++ lang/en/welcome.php
@@ -0,0 +1,13 @@
+<?php 
+
+    return [
+        "heading1"=> "Hello How Are You",
+        "heading2"=> "Welcome to our site",
+        "Home" => "Home",
+        "AboutUs" => "About Us",
+        "Service" => "Service",
+        "Contactus" => "Contact Us",
+
+        "intro" => "Hii everyone this is :name How are you."
+    ];
+?>
